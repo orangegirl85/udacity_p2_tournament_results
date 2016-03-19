b3b99ce6341fb47ee1cc63f9b30a5f620b207992
Nicoleta Ianosi <nicoisonai@yahoo.com>
2016-03-20 00:37:21 +0200

    fix: Fix PEP8 violations

--- tournament.py
+++ tournament.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
-#
+"""Tournament.py - implementation of a Swiss-system tournament."""
 
 import psycopg2
 from psycopg2 import DatabaseError
@@ -13,12 +11,13 @@ def connect():
 
 
 def execute(query, values=()):
-    """Execute a query in the database
+    """Execute a query in the database.
+
     Args:
         query: query to execute
         values: tuple with query values used to avoid sql injection
 
-    Raises DatabaseError if query couldn't be executed
+    Raises DatabaseError if query couldn't be executed.
     """
     db_conn = connect()
     cursor = db_conn.cursor()
@@ -31,7 +30,8 @@ def execute(query, values=()):
 
 
 def fetch_one(query):
-    """Fetch one row from a table according to a query
+    """Fetch one row from a table according to a query.
+
     Args:
         query: query to execute
 
@@ -46,7 +46,8 @@ def fetch_one(query):
 
 
 def fetch_all(query, values=()):
-    """Fetch all rows from a table according to a query
+    """Fetch all rows from a table according to a query.
+
     Args:
         query: query to execute
         values: tuple with query values used to avoid sql injection
@@ -74,13 +75,13 @@ def delete_players():
 
 
 def count_players():
-    """Returns the number of players currently registered."""
+    """Return the number of players currently registered."""
     query = "SELECT COUNT(id) FROM {table_name}".format(table_name='players')
     return fetch_one(query)
 
 
 def register_player(name):
-    """Adds a player to the tournament database.
+    """Add a player to the tournament database.
 
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
@@ -88,15 +89,16 @@ def register_player(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    query = "INSERT INTO {table_name}(name) VALUES (%s)".format(table_name='players')
+    query = "INSERT INTO {table_name}(name) VALUES (%s)".format(
+        table_name='players')
     execute(query, (name,))
 
 
 def player_standings():
-    """Returns a list of the players and their win records, sorted by wins.
+    """Return a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -105,7 +107,6 @@ def player_standings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
     query = '''
         SELECT v1.id, v1.name, v1.wins, v2.matches
         FROM view_player_id_name_won_matches as v1, view_player_matches as v2
@@ -116,18 +117,19 @@ def player_standings():
 
 
 def report_match(winner, loser):
-    """Records the outcome of a single match between two players.
+    """Record the outcome of a single match between two players.
 
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    query = "INSERT INTO {table_name}(winner, loser) VALUES (%s, %s)".format(table_name='matches')
+    query = "INSERT INTO {table_name}(winner, loser) VALUES (%s, %s)".format(
+        table_name='matches')
     execute(query, (winner, loser))
 
 
 def swiss_pairings():
-    """Returns a list of pairs of players for the next round of a match.
+    """Return a list of pairs of players for the next round of a match.
 
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
@@ -141,7 +143,6 @@ def swiss_pairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
     standings = player_standings()
     possible_matches = _get_possible_matches_for_next_round(standings)
     names, wins = _get_names_and_wins(standings)
@@ -156,7 +157,7 @@ def swiss_pairings():
 
 
 def _get_possible_matches_for_next_round(standings):
-    """Returns possible next matches for next round.
+    """Return possible next matches for next round.
 
     Args:
         standings: actual standings for each player
@@ -187,7 +188,7 @@ def _get_possible_matches_for_next_round(standings):
 
 
 def _get_names_and_wins(standings):
-    """Returns 2 dictionaries: one with nr of wins and the other with names of the players.
+    """Return nr of wins and the name for each player.
 
     Args:
         standings: actual standings for each player
@@ -209,7 +210,7 @@ def _get_names_and_wins(standings):
 
 
 def _build_graph(possible_matches, wins):
-    """Returns a list with all combinations of possible next matches and their weight.
+    """Return all possible combinations of next matches and their weight.
 
     Args:
         possible_matches
@@ -226,14 +227,16 @@ def _build_graph(possible_matches, wins):
     graph = []
     for id_player1 in possible_matches:
         for id_player2 in possible_matches[id_player1]:
-            graph.append((abs(wins[id_player1] - wins[id_player2]), id_player1, id_player2))
+            weight = abs(wins[id_player1] - wins[id_player2])
+            graph.append(
+                (weight, id_player1, id_player2))
 
     graph.sort()
     return graph
 
 
 def _get_next_matches_ids(graph):
-    """ Finds next pairs of matches taking considering the win record of the players
+    """Find best pairs of matches for next round.
 
     Args:
         graph: a list of tuples that contains every possible next match
@@ -260,7 +263,7 @@ def _get_next_matches_ids(graph):
 
 
 def find_path(graph, start, nr_players, nodes, path=set(), count=0):
-    """
+    """Determine a set of possible matches for a round.
 
     Args:
         graph: all possible matches
@@ -273,7 +276,6 @@ def find_path(graph, start, nr_players, nodes, path=set(), count=0):
     Returns:
         count/new_count - the sum of all chosen matches weights
         path/new_path  - a list with all matches
-
     """
     weight, id_player1, id_player2 = start
     nodes.append(id_player1)
@@ -286,7 +288,8 @@ def find_path(graph, start, nr_players, nodes, path=set(), count=0):
     for edge in graph:
         weight1, id_player1, id_player2 = edge
         if id_player1 not in nodes and id_player2 not in nodes:
-            new_count, new_path = find_path(graph, edge, nr_players, nodes, path, count)
+            new_count, new_path = find_path(
+                graph, edge, nr_players, nodes, path, count)
             if new_path:
                 return new_count, new_path
 
--- tournament_test.py
+++ tournament_test.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
-#
-# Test cases for tournament.py
+"""Test cases for tournament.py."""
 
 import unittest
 
-from tournament import *
+from tournament import delete_matches, delete_players, register_player, \
+    count_players, player_standings, swiss_pairings, report_match
+
 from math import log
 from psycopg2 import DatabaseError
 
 
 class TournamentTestBasic(unittest.TestCase):
+    """Basic Tournament Class."""
+
     def setUp(self):
-        """
-        Before each test we clean the database.
-        We reset the matches and players.
+        """Clean database before each test.
+
+        Reset the matches and players.
         """
         delete_matches()
         delete_players()
 
-    def _registerPlayers(self, players):
-        """
-        Registers a list of players in the database
+    def _register_players(self, players):
+        """Register a list of players in the database.
 
         Args:
             players
@@ -31,139 +33,174 @@ class TournamentTestBasic(unittest.TestCase):
 
 
 class TournamentTestCount(TournamentTestBasic):
-    def testCountAfterPlayersAreDeleted(self):
-        """
-        Test player count after players are deleted.
-        """
-        c = self._assertCountPlayers(0, "After deletion, count_players should return zero.")
-        self.assertNotEqual(c, '0', "count_players should return numeric zero, not string '0'.")
-        print "1. count_players() returns 0 after initial deletePlayers() execution."
+    """Tournament Test Count."""
 
-    def testCountAfterPlayersAreRegistered(self):
-        """
-        Test player count after 1 and 2 players registered.
-        """
+    def test_count_after_players_are_deleted(self):
+        """Test player count after players are deleted."""
+        c = self._assert_count_players(
+            0, "After deletion, count_players should return zero.")
+        msg_error = "count_players should return numeric zero, not string '0'."
+        self.assertNotEqual(c, '0', msg_error)
+        msg = "1. count_players() returns 0 after initial"
+        print msg + " delete_players() execution."
+
+    def test_count_after_players_are_registered(self):
+        """Test player count after 1 and 2 players registered."""
         register_player("Chandra Nalaar")
-        self._assertCountPlayers(1, "After one player registers, count_players() should be 1.")
+        self._assert_count_players(
+            1, "After one player registers, count_players() should be 1.")
         print "2. count_players() returns 1 after one player is registered."
 
         register_player("Jace Beleren")
-        self._assertCountPlayers(2, "After two players register, count_players() should be 2.")
+        self._assert_count_players(
+            2, "After two players register, count_players() should be 2.")
         print "3. count_players() returns 2 after two players are registered."
 
         delete_players()
-        self._assertCountPlayers(0, "After deletion, count_players should return zero.")
-        print "4. count_players() returns zero after registered players are deleted.\n" \
-              "5. Player records successfully deleted."
+        self._assert_count_players(
+            0, "After deletion, count_players should return zero.")
+        msg = "4. count_players() returns zero after "
+        print msg + "registered players are deleted.\n" \
+            "5. Player records successfully deleted."
 
-    def _assertCountPlayers(self, expected_count_players, message):
+    def _assert_count_players(self, expected_count_players, message):
         c = count_players()
-        self.assertEqual(c, expected_count_players, message + " Got {c}".format(c=c))
+        self.assertEqual(c, expected_count_players,
+                         message + " Got {c}".format(c=c))
 
         return c
 
 
 class TournamentTestStandings(TournamentTestBasic):
-    def testStandingsBeforeMatches(self):
+    """Tournament Standings Test."""
+
+    def test_standings_before_matches(self):
+        """Test to ensure players are properly represented in standings.
+
+        Standings are analysed prior to any matches being reported.
         """
-        Test to ensure players are properly represented in standings prior
-        to any matches being reported.
-        """
-        self._registerPlayers(["Melpomene Murray", "Randy Schwartz"])
+        self._register_players(["Melpomene Murray", "Randy Schwartz"])
         standings = player_standings()
-        self._assertStandingsLength(standings)
-        self._assertStandingsValues(standings)
+        self._assert_standings_length(standings)
+        self._assert_standings_values(standings)
+        msg = "6. Newly registered players appear"
+        print msg + " in the standings with no matches."
 
-        print "6. Newly registered players appear in the standings with no matches."
+    def _assert_standings_length(self, standings):
+        self.assertFalse(len(standings) < 2, "Players should appear in \
+            playerStandings even before they have played any matches.")
+        self.assertFalse(len(standings) > 2,
+                         "Only registered players should appear in standings.")
+        self.assertTrue(
+            len(standings[0]) == 4, "Each playerStandings row should have four \
+             columns.")
 
-    def _assertStandingsLength(self, standings):
-        self.assertFalse(len(standings) < 2, "Players should appear in playerStandings even before "
-                                             "they have played any matches.")
-        self.assertFalse(len(standings) > 2, "Only registered players should appear in standings.")
-        self.assertTrue(len(standings[0]) == 4, "Each playerStandings row should have four columns.")
-
-    def _assertStandingsValues(self, standings):
-        [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
-        self.assertTrue(matches1 == 0 and matches2 == 0, "Newly registered players should have no matches.")
-        self.assertTrue(wins1 == 0 and wins2 == 0, "Newly registered players should have no wins.")
-        self.assertEqual(set([name1, name2]), set(["Melpomene Murray", "Randy Schwartz"]),
-                         "Registered players' names should appear in standings, "
-                         "even if they have no matches played.")
+    def _assert_standings_values(self, standings):
+        [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = \
+            standings
+        self.assertTrue(matches1 == 0 and matches2 == 0,
+                        "Newly registered players should have no matches.")
+        self.assertTrue(wins1 == 0 and wins2 == 0,
+                        "Newly registered players should have no wins.")
+        self.assertEqual(set([name1, name2]),
+                         set(["Melpomene Murray", "Randy Schwartz"]),
+                         "Registered players' names should appear in standings, \
+            even if they have no matches played.")
 
 
 class TournamentTestTReportMatches(TournamentTestBasic):
-    def testReportMatches(self):
-        """
-        Test that matches are reported properly.
+    """Tournament Report Matches Test."""
+
+    def test_report_matches(self):
+        """Test that matches are reported properly.
+
         Test to confirm matches are deleted properly.
         """
-        self._registerPlayers(["Bruno Walton", "Boots O'Neal", "Cathy Burton", "Diane Grant"])
+        self._register_players(
+            ["Bruno Walton", "Boots O'Neal", "Cathy Burton", "Diane Grant"])
 
         standings = player_standings()
         [id1, id2, id3, id4] = [row[0] for row in standings]
         report_match(id1, id2)
         report_match(id3, id4)
-        self._assertStandingsValuesAfterFirstRound(id1, id2, id3, id4)
+        self._assert_standings_values_after_first_round(id1, id2, id3, id4)
         print "7. After a match, players have updated standings."
 
         delete_matches()
-        self._assertStandingsValuesAfterDeleteMatches()
+        self._assert_standings_values_after_delete_matches()
         print "8. After match deletion, player standings are properly reset."
         print "9. Matches are properly deleted."
 
-    def _assertStandingsValuesAfterFirstRound(self, id1, id2, id3, id4):
+    def _assert_standings_values_after_first_round(self, id1, id2, id3, id4):
         standings = player_standings()
         for (i, n, w, m) in standings:
-            self.assertEqual(m, 1, "Each player should have one match recorded.")
+            self.assertEqual(
+                m, 1, "Each player should have one match recorded.")
             if i in (id1, id3):
-                self.assertTrue(w == 1, "Each match winner should have one win recorded.")
+                self.assertTrue(
+                    w == 1, "Each match winner should have one win recorded.")
             if i in (id2, id4):
-                self.assertTrue(w == 0, "Each match loser should have zero wins recorded.")
+                self.assertTrue(
+                    w == 0, "Each match loser should have zero wins recorded.")
 
-    def _assertStandingsValuesAfterDeleteMatches(self):
+    def _assert_standings_values_after_delete_matches(self):
         standings = player_standings()
-        self.assertEqual(len(standings), 4, "Match deletion should not change number of players in standings.")
+        self.assertEqual(len(
+            standings), 4, "Match deletion should not change number of players \
+                 in standings.")
 
         for (i, n, w, m) in standings:
-            self.assertEqual(m, 0, "After deleting matches, players should have zero matches recorded.")
-            self.assertEqual(w, 0, "After deleting matches, players should have zero wins recorded.")
+            self.assertEqual(
+                m, 0, "After deleting matches, players should have zero matches \
+                 recorded.")
+            self.assertEqual(
+                w, 0, "After deleting matches, players should have zero wins \
+                recorded.")
 
 
-class TournamentTestU(TournamentTestBasic):
-    def testPairings(self):
+class TournamentTestUPairings(TournamentTestBasic):
+    """Tournament Pairings Test."""
+
+    def test_pairings(self):
+        """Test that pairings are generated properly.
+
+        Test this before and after match reporting.
         """
-        Test that pairings are generated properly both before and after match reporting.
-        """
-        players = ["Twilight Sparkle", "Fluttershy", "Applejack", "Pinkie Pie", "Rarity",
-                   "Rainbow Dash", "Princess Celestia", "Princess Luna"]
-        self._registerPlayers(players)
+        players = ["Twilight Sparkle", "Fluttershy", "Applejack", "Pinkie Pie",
+                   "Rarity", "Rainbow Dash", "Princess Celestia",
+                   "Princess Luna"]
+        self._register_players(players)
 
         standings = player_standings()
-        [id1, id2, id3, id4, id5, id6, id7, id8] = [row[0] for row in standings]
-        self._assertPairingsLength()
+        [id1, id2, id3, id4, id5, id6, id7, id8] = [row[0]
+                                                    for row in standings]
+        self._assert_pairings_length()
 
-        self._registerMatchesFirstRound(id1, id2, id3, id4, id5, id6, id7, id8)
+        self._register_first_round(id1, id2, id3, id4, id5, id6, id7, id8)
 
-        pairings = self._assertPairingsLength()
-        self._assertPairingsAfterFirstRound(pairings, id1, id2, id3, id4, id5, id6, id7, id8)
+        pairings = self._assert_pairings_length()
+        self._assert_pairings_after_first_round(
+            pairings, id1, id2, id3, id4, id5, id6, id7, id8)
 
         print "10. After one match, players with one win are properly paired."
 
-    def _assertPairingsLength(self):
+    def _assert_pairings_length(self):
         pairings = swiss_pairings()
-        self.assertEqual(len(pairings), 4, "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(
+        self.assertEqual(len(pairings), 4, "For eight players, swissPairings \
+            should return 4 pairs. Got {pairs}".format(
             pairs=len(pairings)))
         return pairings
 
-    def _registerMatchesFirstRound(self, id1, id2, id3, id4, id5, id6, id7, id8):
+    def _register_first_round(self, id1, id2, id3, id4, id5, id6, id7, id8):
         report_match(id1, id2)
         report_match(id3, id4)
         report_match(id5, id6)
         report_match(id7, id8)
 
-    def _assertPairingsAfterFirstRound(self, pairings, id1, id2, id3, id4, id5, id6, id7, id8):
-        [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4), (pid5, pname5, pid6, pname6),
-         (pid7, pname7, pid8, pname8)] = pairings
+    def _assert_pairings_after_first_round(self, pairings, id1, id2, id3, id4,
+                                           id5, id6, id7, id8):
+        [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4),
+         (pid5, pname5, pid6, pname6), (pid7, pname7, pid8, pname8)] = pairings
         possible_pairs = set([frozenset([id1, id3]), frozenset([id1, id5]),
                               frozenset([id1, id7]), frozenset([id3, id5]),
                               frozenset([id3, id7]), frozenset([id5, id7]),
@@ -172,17 +209,24 @@ class TournamentTestU(TournamentTestBasic):
                               frozenset([id4, id8]), frozenset([id6, id8])
                               ])
         actual_pairs = set(
-            [frozenset([pid1, pid2]), frozenset([pid3, pid4]), frozenset([pid5, pid6]), frozenset([pid7, pid8])])
+            [frozenset([pid1, pid2]), frozenset([pid3, pid4]),
+                frozenset([pid5, pid6]), frozenset([pid7, pid8])])
         for pair in actual_pairs:
-            self.assertIn(pair, possible_pairs, "After one match, players with one win should be paired.")
+            self.assertIn(
+                pair, possible_pairs, "After one match, players with one win \
+                should be paired.")
 
 
 class TournamentTestZExtras(TournamentTestBasic):
-    def testPreventRematches(self):
+    """Tournament Test Extras."""
+
+    def test_prevent_rematches(self):
+        """Test that the system does not allow rematches.
+
+        Test that the system prevent matches between a player and himself.
         """
-        Test that the system does not allow rematches or matches between a player and himself
-        """
-        self._registerPlayers(["Bruno Walton", "Boots O'Neal", "Cathy Burton", "Diane Grant"])
+        self._register_players(
+            ["Bruno Walton", "Boots O'Neal", "Cathy Burton", "Diane Grant"])
 
         standings = player_standings()
         [id1, id2, id3, id4] = [row[0] for row in standings]
@@ -194,19 +238,21 @@ class TournamentTestZExtras(TournamentTestBasic):
 
 
 class SwissTournament(object):
+    """Swiss Tournament display."""
+
     NR_OF_PLAYERS = 16
 
     def generate_whole_swiss_tournament(self):
-        """
-        Display each round statistics in a swiss tournament.
+        """Display each round statistics in a swiss tournament.
+
         Display winner's name.
         """
-
         # clean database
         delete_matches()
         delete_players()
 
-        print '\n\nSWISS TOURNAMENT FOR {0} PLAYERS:'.format(self.NR_OF_PLAYERS)
+        print '\n\nSWISS TOURNAMENT FOR {0} PLAYERS:'.format(
+            self.NR_OF_PLAYERS)
 
         # register {NR_OF_PLAYERS} players
         i = 0
@@ -226,7 +272,8 @@ class SwissTournament(object):
             z = 0
             # register next round matches
             while j < self.NR_OF_PLAYERS:
-                (group['pid' + str(j)], group['pname' + str(j)], group['pid' + str(j + 1)],
+                (group['pid' + str(j)], group['pname' + str(j)],
+                 group['pid' + str(j + 1)],
                  group['pname' + str(j + 1)]) = pairings[z]
                 report_match(group['pid' + str(j)], group['pid' + str(j + 1)])
                 j += 2
@@ -244,8 +291,7 @@ class SwissTournament(object):
         print "-------------------------\n\n"
 
     def stats(self):
-        """
-        Display player_standings
+        """Display player_standings.
 
          Id | Name | Wins | Matches
          1 | Player 1 | 1 | 1
@@ -261,8 +307,8 @@ class SwissTournament(object):
         print '----------------------------'
         j = 0
         while j < self.NR_OF_PLAYERS:
-            print str(standings[j][0]) + " | " + str(standings[j][1]) + " | " + str(
-                standings[j][2]) + " | " + str(standings[j][3])
+            print str(standings[j][0]) + " | " + str(standings[j][1]) + " | " \
+                + str(standings[j][2]) + " | " + str(standings[j][3])
             j += 1
 
         return standings[0][1]
